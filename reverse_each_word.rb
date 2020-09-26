758fb763510a47128f7ed600886944af058d0c93
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 20:05:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,1 @@
 def reverse_each_word (elements)
-al_array.join (" ")
-end
-  
-      initial_array = string.split (" ")
-  new_array = []
-  initial_array.each do |elements|
-    flipped_array.reverse 
-  end 
-  initial_array.push elements
-  initi
