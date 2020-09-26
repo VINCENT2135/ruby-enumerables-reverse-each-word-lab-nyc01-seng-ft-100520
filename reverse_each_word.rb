b9869399e0ddf0fb5c1cdf381a4013bae3fa6b8f
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 20:16:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,12 @@ end
 end 
 
 
+def reverse_each_word (sentence_array)
+sentence_array = sentence_array.split(" ")
+new_sentence_array = sentence_array.collect do |word|
+   word.reverse
+end
+
+ new_sentence = new_sentence_array.join(" ")
+end 
+
