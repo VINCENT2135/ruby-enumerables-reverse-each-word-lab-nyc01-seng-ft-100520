1692106c1b910340773864e62a913d280e511522
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 20:05:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word (elements)
-  initial_array = string.split (" ")
+al_array.join (" ")
+end
+  
+      initial_array = string.split (" ")
   new_array = []
   initial_array.each do |elements|
     flipped_array.reverse 
   end 
   initial_array.push elements
-  initial_array.join (" ")
-end
-  
-    
+  initi
